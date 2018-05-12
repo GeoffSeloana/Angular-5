96b024b59e0241ef273f335d38f3e09636b09eea
geoffie <geoff.seloana@gmail.com>
2018-05-13 00:30:22 +0200

    Added movies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
