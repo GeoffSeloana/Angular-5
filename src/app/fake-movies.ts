d92769554ecfebc01f8fc1043c8d1f8f73c23e60
geoffie <geoff.seloana@gmail.com>
2018-05-13 18:57:14 +0200

    Added movies component

---
+++ src/app/fake-movies.ts
@@ -0,0 +1,26 @@
+
+
+import {Movie} from '../models/movie';
+
+export let fakeMovie: Movie[] = [
+  {
+    id: 1,
+    name: 'The Ghost and the darkness',
+    releaseYear: 2001
+  },
+  {
+    id: 2,
+    name: 'The Ghost and the darkness',
+    releaseYear: 2001
+  },
+  {
+    id: 3,
+    name: 'The Ghost and the darkness',
+    releaseYear: 2001
+  },
+  {
+    id: 4,
+    name: 'The Ghost and the darkness',
+    releaseYear: 2001
+  },
+];
