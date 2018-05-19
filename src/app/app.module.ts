f738ebbab80238da1df16d4031cd50b885ad30c8
geoffie <geoff.seloana@gmail.com>
2018-05-19 18:39:11 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+
+import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { FormsModule } from '@angular/forms';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieService} from './movie.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
